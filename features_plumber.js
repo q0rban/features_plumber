6b13267e768d575849620a315f0cd9c6ab01d271
jsansbury <jsansbury@lullabot.com>
2011-01-14 15:40:35 -0500

    Yay! Working form.

--- features_plumber.js
+++ features_plumber.js
@@ -7,14 +7,21 @@
 
 Drupal.behaviors.featuresPlumber = function(context) {
   // By not filtering to context, this will call every time.
-  var table = $('#features-export-populated');
+  Drupal.settings.featuresPlumberAction = Drupal.settings.featuresPlumberAction || $('#features-export-form').attr('action');
+  var action = Drupal.settings.featuresPlumberAction;
   var form = $('#features-plumber-form');
 
   //form.children().unwrap();
-  $('.features-plumber-plumbable', table).each(function () {
+  $('.features-plumber-plumbable', form).each(function () {
     var item = $(this);
     var component = item.attr('component');
     var componentVal = item.attr('component_val');
     $('input[component="' + component + '"][component_val="' + componentVal + '"]', form).prependTo(item);
+  });
+
+  // Form within a form. Eek!
+  $('#features-export-form').submit(function () {
+    form.remove();
+    $(this).attr('action', action);
   });
 }
