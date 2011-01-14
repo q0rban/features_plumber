f4a45d2ad47ba6799f9a4a1d894b254fe25c1f7e
jsansbury <jsansbury@lullabot.com>
2011-01-14 17:06:41 -0500

    Move UI stuff to separate module.

--- features_plumber_ui/features_plumber_ui.js
+++ features_plumber_ui/features_plumber_ui.js
@@ -5,14 +5,14 @@
  * Features Plumber js.
  */
 
-Drupal.behaviors.featuresPlumber = function(context) {
+Drupal.behaviors.featuresPlumberUi = function(context) {
   // By not filtering to context, this will call every time.
-  Drupal.settings.featuresPlumberAction = Drupal.settings.featuresPlumberAction || $('#features-export-form').attr('action');
-  var action = Drupal.settings.featuresPlumberAction;
-  var form = $('#features-plumber-form');
+  Drupal.settings.featuresPlumberUiAction = Drupal.settings.featuresPlumberUiAction || $('#features-export-form').attr('action');
+  var action = Drupal.settings.featuresPlumberUiAction;
+  var form = $('#features-plumber-ui-form');
 
   //form.children().unwrap();
-  $('.features-plumber-plumbable', form).each(function () {
+  $('.features-plumber-ui-plumbable', form).each(function () {
     var item = $(this);
     var component = item.attr('component');
     var componentVal = item.attr('component_val');
